596fcebcfa9b3c6918c1a00e391b1a8445c33b20
Jonhy-D <jonatan.l.r.o.d@gmail.com>
2024-07-04 18:59:49 -0600

    Styling item cards

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -13,10 +13,10 @@ export class ItemListComponent {
   items = [{
       id: 1,
       title: 'Thor love and thunder',
-      description: 'ajklniofnoadfklasjmfkla',
+      description: 'The film follows Thor (Chris Hemsworth) on a journey unlike anything he has faced before: a search for inner peace.',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBOyZD9CL1eGq1pKq4tPcXp6KX-iw5J2krnA&s',
-      duration: '',
-      genere: '',
+      duration: '160 min',
+      genre: 'Action',
       type: ''
     },
     {
@@ -24,8 +24,8 @@ export class ItemListComponent {
       title: 'Kingdom of the Planet of the Apes',
       description: 'ajklniofnoadfklasjmfkla',
       image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8wqa3LtQG2hYDOLlJ20Jgy0yoiKPkvcw_qQ&s',
-      duration: '',
-      genere: '',
+      duration: '152 min',
+      genre: 'Fiction',
       type: ''
     }
   ]
